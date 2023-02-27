a0afde8288214c756456cd25ff5e058494a46067
jaime-ribeiro <littlejou@gmail.com>
2023-02-26 22:54:34 -0300

    Erasing files with .jsx extensions :fire:

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,18 @@
+import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css'
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
     hasBorder?: boolean;
-    src: string;
-    alt?: string;
 }
 
-export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
     return (
         <img
             className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-            src={src}
-            alt={alt}
+            {...props}
+        // uma forma de passar os valores src e alt pra dentro da tag img sem escrever elas diretamente, apenas pegando o resto com "..." rest operator
+
         />
     );
 }
+
